4e93d162c8d4124605d1b216b25f6af6a2e46e4b
Jonathan Peralez <jperalez@gmail.com>
2019-10-17 16:24:42 -0700

    Add comments to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,17 @@ module.exports = {
   mode: 'development',
   devServer: {
     hot: true,
+    // bundled files are available in the browser under this path
     publicPath: '/build/',
     proxy: {
       '/api': 'http://localhost:3000',
     },
     historyApiFallback: true,
+    /* tells server where you want to serve files from
+    * only necessary if you want to serve static files
+    * publicPath takes precedence
+    * By default it will use your current working directory to serve content
+    * to disable contentBase set it to false */
     contentBase: path.join(__dirname, 'build'),
   },
   devtool: 'source-map',
@@ -29,7 +35,10 @@ module.exports = {
     ],
   },
   output: {
+    // where to store the bundle file after it's compiled
     path: path.resolve(__dirname, 'build'),
+    // public URL of the output directory when referenced in a directory
+    // publicPath: '/',
     publicPath: '/build/',
     filename: 'bundle.js',
   },
