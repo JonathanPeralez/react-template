d250016f2fb507176511caa98a1faef4c8939e3b
Jonathan Peralez <jperalez@gmail.com>
2019-10-18 09:03:51 -0700

    Update webpack.config.js
    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './react/index.js',
   mode: 'development',
+  // mode: 'production',
   devServer: {
-    hot: true,
     // bundled files are available in the browser under this path
-    publicPath: '/build/',
+    publicPath: '/',
     proxy: {
       '/api': 'http://localhost:3000',
     },
     historyApiFallback: true,
-    /* tells server where you want to serve files from
-    * only necessary if you want to serve static files
-    * publicPath takes precedence
-    * By default it will use your current working directory to serve content
-    * to disable contentBase set it to false */
-    contentBase: path.join(__dirname, 'build'),
+    /*
+    * Tells server where you want to serve files from
+    * - only necessary if you want to serve static files
+    * - Determines where the bundles should be served from
+    * - By default it will use your current working directory to serve content
+    * - To disable contentBase set it to false
+    */
+    contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'assets')],
+    // contentBase: path.join(__dirname, 'dist')
   },
   devtool: 'source-map',
   module: {
@@ -32,15 +36,30 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
     ],
   },
   output: {
     // where to store the bundle file after it's compiled
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     // public URL of the output directory when referenced in a directory
-    // publicPath: '/',
-    publicPath: '/build/',
+    publicPath: '/',
     filename: 'bundle.js',
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Built by Webpack',
+      template: './react/index.html',
+      filename: path.join(__dirname, './dist/index.html')
+    })
+  ],
 };
