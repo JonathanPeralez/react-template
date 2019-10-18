2b80b122bd000403ca0993bd5d7d08086a0499d1
Jonathan Peralez <jperalez@gmail.com>
2019-10-18 10:52:35 -0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './react/index.js',
-  mode: 'development',
-  // mode: 'production',
+  // mode: 'development',
+  mode: 'production',
   devServer: {
     // bundled files are available in the browser under this path
     publicPath: '/',
@@ -20,7 +21,7 @@ module.exports = {
     * - By default it will use your current working directory to serve content
     * - To disable contentBase set it to false
     */
-    contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'assets')],
+    contentBase: [ path.join(__dirname, 'dist'), path.join(__dirname, 'assets') ],
     // contentBase: path.join(__dirname, 'dist')
   },
   devtool: 'source-map',
@@ -32,18 +33,33 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [ '@babel/preset-env', '@babel/preset-react' ],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [ 'style-loader', 'css-loader' ],
       },
+      // Simple file-loader - outputs files to a directory
+      // {
+      //   test: /\.(png|jp(e*)g|gif|svg)$/,
+      //   use: ['file-loader']
+      // },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      }
+        // If an image is small, load the file into the bundle
+        // If it's larger, use file-loader
+        test: /\.(png|jp(e*)g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000,
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   output: {
@@ -59,7 +75,13 @@ module.exports = {
       hash: true,
       title: 'Built by Webpack',
       template: './react/index.html',
-      filename: path.join(__dirname, './dist/index.html')
-    })
+      filename: '../dist/index.html',
+    }),
+    // for copying files from one folder to another
+    // not necessary when using the url-loader above
+    // new CopyWebpackPlugin([ {
+    //   from: 'assets',
+    //   to: 'images',
+    // } ]),
   ],
 };
