dfa039d34d919a162aeb54a3c6c8f5ac232b4bf3
Jonathan Peralez <jperalez@gmail.com>
2019-11-05 15:12:36 -0800

    add normalize.css and local css reset

--- react/index.js
+++ react/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import 'normalize.css';
+import './local.reset.css';
 import App from './components/App';
 
 render(
   <div>
     <App />
   </div>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
